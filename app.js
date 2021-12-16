6b49a8df8ce135fea1670bed85327b44cc124039
Hulff <ph2717693@Gmail.com>
2021-12-16 11:29:33 -0300

    nada que importa

--- app.js
+++ app.js
@@ -16,22 +16,22 @@ const orange = document.getElementById('orange')
 
 let estado = "finished"
 
-const listaCores = [red,yellow,blue,green,purple,orange]
+const listaCores = [red, yellow, blue, green, purple, orange]
 
 
 let listaGerada = [];
 let listaJogador = [];
 let rodada = 0
 let clicksRestantes = 0
-let recorde = 0 
+let recorde = 0
 
 let width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 let height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
-if (width > 980 ) {
+if (width > 980) {
   console.log(width)
   col3.style.flexDirection = 'column'
-  orange.style.marginLeft = '-10px'  
+  orange.style.marginLeft = '-10px'
 }
 
 document.addEventListener('keydown', function (event) {
@@ -48,7 +48,7 @@ document.addEventListener('keydown', function (event) {
     adicionarNumeroDoJogador(4)
   }
   if (event.keyCode === 13) {
-    if (start.style.visibility != 'hidden'){
+    if (start.style.visibility != 'hidden') {
       começar()
     }
   }
@@ -59,19 +59,19 @@ orange.classList.add('hidden')
 
 hard.classList.add('disabled')
 
-hard.addEventListener('click' , check())
+hard.addEventListener('click', check())
 
-function check () {
+function check() {
   if (hard.classList[1] == 'disabled') {
     console.log('complete a rodada 20 para liberar')
   } else {
-    
+
   }
 }
 
 
-function sortearNumero (n) {
-  let numeroSortedo = ((Math.floor(Math.random() * n))+1)
+function sortearNumero(n) {
+  let numeroSortedo = ((Math.floor(Math.random() * n)) + 1)
   gerarLista(numeroSortedo)
   return numeroSortedo, listaGerada
 }
@@ -80,21 +80,21 @@ function gerarLista(numeroSortedo) {
   return listaGerada
 }
 function adicionarNumeroDoJogador(numero) {
-  if ( estado == "finished") {
+  if (estado == "finished") {
     listaJogador.push(numero)
     acenderDiv(numero)
-    if( clicksRestantes != 0 && compararListas() == true  ) {
+    if (clicksRestantes != 0 && compararListas() == true) {
       reduzirClicks()
-    } 
+    }
   } else if (estado == "reading") {
     console.log("reproduzindo sequencia")
   }
 }
 function compararListas() {
-  let i = listaJogador.length-1
-  if(listaGerada[i]==listaJogador[i]) {
-     console.log('bateu')
-     return true
+  let i = listaJogador.length - 1
+  if (listaGerada[i] == listaJogador[i]) {
+    console.log('bateu')
+    return true
   } else {
     console.log('errou')
     reset()
@@ -106,64 +106,64 @@ function compararListas() {
 
 function reset() {
   listaGerada = []
-  if (recorde<rodada) {
-  recorde = rodada
-  salvarRecorde(recorde)
-  if(recorde>19) {
-    hard.classList.remove('disabled')
-  }
-  } else if (recorde> rodada) {
+  if (recorde < rodada) {
+    recorde = rodada
+    salvarRecorde(recorde)
+    if (recorde > 19) {
+      hard.classList.remove('disabled')
+    }
+  } else if (recorde > rodada) {
     salvarRecorde(recorde)
   }
   rodada = 0
   clicksRestantes = 0
 
-  return rodada,clicksRestantes,listaGerada,recorde
+  return rodada, clicksRestantes, listaGerada, recorde
 }
-function resetText () {
+function resetText() {
   round.innerText = ''
   clicks.innerText = ''
   start.classList.remove('disabled')
 }
-function salvarRecorde (recorde) {
-  recordeText.innerHTML = "Maior Rodada:"+recorde
+function salvarRecorde(recorde) {
+  recordeText.innerHTML = "Maior Rodada:" + recorde
 }
-function começar () {
-  
+function começar() {
+
   recordeText.innerHTML = ""
 
   rodada++
   clicksRestantes = rodada
 
-  atualizarTexto(rodada,clicksRestantes)
+  atualizarTexto(rodada, clicksRestantes)
 
   sortearNumero(4)
 
-  setTimeout(()=>{lerLista()},500)
+  setTimeout(() => { lerLista() }, 500)
 
   listaJogador = [];
 
   start.classList.add('disabled')
 
-  return rodada,clicksRestantes,listaJogador
+  return rodada, clicksRestantes, listaJogador
 }
-function atualizarTexto (numeroDaRodada,clicksRestantes) {
-  round.innerText = "Round:"+ numeroDaRodada
-  clicks.innerText = "Clicks Restantes:"+ clicksRestantes
+function atualizarTexto(numeroDaRodada, clicksRestantes) {
+  round.innerText = "Round:" + numeroDaRodada
+  clicks.innerText = "Clicks Restantes:" + clicksRestantes
 }
-function reduzirClicks () {
+function reduzirClicks() {
   clicksRestantes--
-  clicks.innerText = "Clicks Restantes:"+ clicksRestantes
+  clicks.innerText = "Clicks Restantes:" + clicksRestantes
   if (clicksRestantes == 0) {
     começar()
   }
 }
-function lerLista () {
-  for (let i=0;i<listaGerada.length;i++) {
+function lerLista() {
+  for (let i = 0; i < listaGerada.length; i++) {
     let numero = listaGerada[i]
-    loop(i,numero)
+    loop(i, numero)
     status.classList.add("visible")
-    
+
     estado = "reading"
 
   }
@@ -171,23 +171,23 @@ function lerLista () {
     estado = "finished"
     status.classList.remove("visible");
     return estado
-  }, ((1000*rodada)+500))
+  }, ((1000 * rodada) + 500))
 }
-function loop(i,numero) {
-  setTimeout(()=> {
+function loop(i, numero) {
+  setTimeout(() => {
     setTimeout(() => {
       acenderDiv(numero)
-    },700)
+    }, 700)
   }, 1000 * i);
 }
 
 function acenderDiv(numero) {
-      listaCores[numero-1].classList.add('ativo')
-      setTimeout(() => {
-      listaCores[numero-1].classList.remove('ativo')
-      }, 300);
+  listaCores[numero - 1].classList.add('ativo')
+  setTimeout(() => {
+    listaCores[numero - 1].classList.remove('ativo')
+  }, 300);
 }
-function mostrarNovasCores () {
+function mostrarNovasCores() {
   purple.style.display = "initial"
   orange.style.display = "initial"
 
